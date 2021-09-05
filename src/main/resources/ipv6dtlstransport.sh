0a95881a4bf7fc938b329d6b8dc19d32000bfb95
Andreas Feldner <pelzi@flying-snail.de>
2021-09-05 12:19:58 +0200

    Startup script adjusted to elimination if tuntopipe process

--- src/main/resources/ipv6dtlstransport.sh
+++ src/main/resources/ipv6dtlstransport.sh
@@ -2,16 +2,14 @@
 mydir="$(dirname "$0")"
 mydir="${mydir/#./$PWD/.}"
 mydir="${mydir/\/bin//lib}"
-tmpdir="$(mktemp -d)"
+tmpdir="$(mktemp -d)" || { echo "Could not create temporary directory" >&2; exit 1; }
 cd "$tmpdir"
-mkfifo -m 600 fromdevice || exit 1
-mkfifo -m 600 todevice || exit 2
-mkfifo -m 600 toayiya || exit 3
+mkfifo -m 600 fromayiya || { echo "Could not create fromayiya fifo" >&2; exit 2; }
+mkfifo -m 600 toayiya || { echo "Could not create toayiya fifo" >&2; exit 3; }
 cp "${mydir}"/logging*.properties "${tmpdir}"
-tuntopipe -i tun0 -u < todevice > fromdevice 2> ${mydir}/tuntopipe.log &
-java -Djava.util.logging.config.file="${mydir}/logging_dtls.properties" -jar "${mydir}/ipv6dtlstransport-1.3.1-SNAPSHOT.jar"  fromdevice todevice > toayiya &
-java -Djava.util.logging.config.file="${mydir}/logging_ayiya.properties" -jar "${mydir}/ipv6transport-1.3.1-SNAPSHOT.jar" toayiya todevice
+java -Djava.util.logging.config.file="${mydir}/logging_dtls.properties" -jar "${mydir}/ipv6dtlstransport-1.3.1-SNAPSHOT.jar"  fromayiya toayiya &
+java -Djava.util.logging.config.file="${mydir}/logging_ayiya.properties" -jar "${mydir}/ipv6transport-1.3.1-SNAPSHOT.jar" toayiya fromayiya
 retcode=$?
-echo "Transporter finished with return code $retcode"
-kill %1 %2
+echo "Transporter ayiya finished with return code $retcode"
+kill %1
 exit $retcode
